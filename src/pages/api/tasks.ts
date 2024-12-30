829edee7f0ca9434169d1c13bf53050593f686fa
Lousenbairm <kohjunxian00@gmail.com>
2024-12-30 17:13:21 +0800

    delete and get req

--- src/pages/api/tasks.ts
+++ src/pages/api/tasks.ts
@@ -3,7 +3,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient
 
+
+async function getTaskList() {
+    return await prisma.task.findMany()
+}
+
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
 
     if(req.method === 'POST') {
         const {task, desc, status} = req.body;
@@ -22,12 +29,46 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 },
             });
 
-            return res.status(200).json(newTask);
+            const taskList = await getTaskList()
+
+            return res.status(200).json(taskList);
 
         } catch (error) {
             console.error("Error creating task", error)
 
             return res.status(500).json({error: `Method ${req.method} not allowed`})
+        }
+    } else if (req.method === 'GET') {
+
+        try {
+            
+            const taskList = await getTaskList()
+            if(taskList.length !== 0) {
+            
+                return res.status(200).json(taskList);
+            } else {
+                return res.status(200).json({message: "DB is empty"});
+            }
+
+        } catch (error) {
+            
+            console.error("Error accessing db", error)
+
+            return res.status(500).json({error: `${error}`})
+        }
+    } else if (req.method === 'DELETE') {
+
+        try {
+
+            await prisma.task.deleteMany({})
+
+            return res.status(200).json({message: "DB has been reset successfully"})
+
+        } catch (error) {
+
+            console.error("Cannot delete db", error)
+
+            return res.status(500).json({error: `${error}`})
         }
     }
 
